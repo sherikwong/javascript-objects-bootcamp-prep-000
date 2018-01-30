f99be95d913de00b94591371de25789c6ffb7c0b
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-30 18:28:34 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+const playlist = {};
+
+function updatePlaylist(object, string1, string2) {
+  object[string1] = string2;
+  return object;
+}
+
+function removeFromPlaylist(object, string){
+  delete object[string];
+  return object;
+}
